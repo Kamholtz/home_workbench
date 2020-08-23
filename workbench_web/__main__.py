b13747dc81a39d07fd0d516079efcf2766385b09
Carl <Carl.Kamholtz@gmail.com>
2020-08-23 14:00:55 +1000

    Site accessible on local network

--- workbench_web/__main__.py
+++ workbench_web/__main__.py
@@ -1,7 +1,7 @@
 import uvicorn
 
-from workbench_web.main import app
+from workbench_web.workbench_web import app
 
 # pipenv run uvicorn main:app --reload --host 0.0.0.0
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
